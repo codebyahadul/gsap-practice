54a3c11347103cddb104250e40734bd976ac0747
codebyahadul <ahadul.prof@gmail.com>
2024-09-28 11:17:40 +0600

    added gsap with react

---
+++ gsap-with-react/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <main className="max-w-6xl mx-auto">
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
---
+++ gsap-with-react/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap'
+import { useRef } from 'react';
+const Navbar = () => {
+    const logoRef = useRef()
+    const tl = gsap.timeline()
+    useGSAP(() => {
+        tl.from(logoRef.current, {
+            y: -30,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2,
+        })
+        tl.from('.linkList h2', {
+            y: -30,
+            opacity: 0,
+            stagger: 0.3
+        })
+    })
+    return (
+        <nav className="flex justify-between items-center my-5">
+            <div>
+                <h1 ref={logoRef} className="text-xl md:text-2xl font-bold">GSAP With React</h1>
+            </div>
+            <div className="flex items-center gap-8 *:cursor-pointer text-lg font-medium linkList">
+                <h2>Home</h2>
+                <h2>Products</h2>
+                <h2>Contact</h2>
+                <h2>About Us</h2>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
